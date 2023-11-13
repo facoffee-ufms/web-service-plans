5bd8aa3a0eb3f22edeef493e05c978cbc017167c
jpwakugawa <jp_wakugawa@outlook.com>
2023-11-13 19:40:57 -0400

    add swagger for plans

--- src/plans/entities/plan.entity.ts
+++ src/plans/entities/plan.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
@@ -5,15 +6,22 @@ export type PlanDocument = HydratedDocument<Plan>;
 
 @Schema()
 export class Plan {
+  @ApiProperty({ example: 'basic', description: 'The name of the Plan' })
   @Prop()
   name: string;
 
+  @ApiProperty({ example: 20, description: 'The price of the Plan' })
   @Prop()
   price: number;
 
+  @ApiProperty({ example: 30, description: 'The duration days of the Plan' })
   @Prop()
   durationDays: number;
 
+  @ApiProperty({
+    example: [{ name: 'coffee', count: 3 }],
+    description: 'The items of the Plan',
+  })
   @Prop()
   items: [
     {
